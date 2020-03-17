58785963f1cd3d8b5eeb7e93d10931d3fb2e7c57
Ian Stewart <istewart@sailthru.com>
2020-03-17 15:28:02 +1300

    Update version to 4.1.0-beta for release

--- android/src/main/java/com/reactlibrary/RNCarnivalModule.java
+++ android/src/main/java/com/reactlibrary/RNCarnivalModule.java
@@ -107,7 +107,7 @@ public class RNCarnivalModule extends ReactContextBaseJavaModule implements Carn
 
             setWrapperMethod = Carnival.class.getDeclaredMethod("setWrapper", cArg);
             setWrapperMethod.setAccessible(true);
-            setWrapperMethod.invoke(null, "React Native", "4.0.0");
+            setWrapperMethod.invoke(null, "React Native", "4.1.0-beta");
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
