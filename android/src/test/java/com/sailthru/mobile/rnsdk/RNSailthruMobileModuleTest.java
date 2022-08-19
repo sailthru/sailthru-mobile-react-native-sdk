981d8642beb99c4f3888038b5feece837d342d1d
Ian Stewart <32638123+igstewart3@users.noreply.github.com>
2022-08-19 14:58:54 +1200

    fix: Update build tools and remove option causing error (#82)

--- android/src/test/java/com/sailthru/mobile/rnsdk/RNSailthruMobileModuleTest.java
+++ android/src/test/java/com/sailthru/mobile/rnsdk/RNSailthruMobileModuleTest.java
@@ -41,10 +41,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
