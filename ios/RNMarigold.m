67ca2adc19becfe23c5eda20cf114356d96054c7
Angelo Esquibel <angelo.esquibel@cheetahdigital.com>
2024-06-26 10:00:26 +0800

    [MP-1832] React Native iOS: Adds wrapper for getDeviceDetails
    This PR:
    Adds a wrapper for the device details to expose to the RN layer
    
    https://sailthru.atlassian.net/browse/MP-1832

--- ios/RNMarigold.m
+++ ios/RNMarigold.m
@@ -2,6 +2,7 @@
 #import "RNMarigold.h"
 #import "RNMessageStream.h"
 #import <UserNotifications/UserNotifications.h>
+#import <objc/runtime.h>
 
 @interface Marigold ()
 
@@ -109,6 +110,11 @@ RCT_EXPORT_METHOD(setInAppNotificationsEnabled:(BOOL)enabled) {
     [self.marigold setInAppNotificationsEnabled:enabled];
 }
 
+RCT_REMAP_METHOD(getDeviceDetails, resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+    NSString *endpoint = [RNMarigold getDeviceDetails];\
+    resolve(endpoint);
+}
+
 #pragma mark - Helper Functions
 
 + (void)rejectPromise:(RCTPromiseRejectBlock)reject withError:(NSError *)error {
@@ -123,6 +129,81 @@ RCT_EXPORT_METHOD(setInAppNotificationsEnabled:(BOOL)enabled) {
     } else {
         [[NSOperationQueue mainQueue] addOperationWithBlock:block];
     }
+}
+
++ (NSString *)getDeviceDetails {
+    RNMarigold *marigold = [RNMarigold new];
+    NSString *endpoint = [marigold getEndpoint];
+    NSString *appId = [marigold getAppId];
+    NSString *deviceId = [marigold getDeviceId];
+    NSString *deviceString = [marigold getDevice];
+    
+    NSString *detailsString = [NSString stringWithFormat:@"Endpoint: %@\nApp ID: %@\nDevice ID: %@\nDevice: %@", endpoint, appId, deviceId, deviceString];
+    
+    return detailsString;
+}
+
+- (NSString *)getEndpoint {
+    Class apiClientClass = objc_getClass("CPAPIClient");
+    SEL sharedSelector = NSSelectorFromString(@"sharedClient");
+    IMP sharedImp = [apiClientClass methodForSelector:sharedSelector];
+    id (*sharedFunc)(id, SEL) = (id (*)(id, SEL))sharedImp;
+    id apiClient = sharedFunc(apiClientClass, sharedSelector);
+    
+    SEL urlSelector = NSSelectorFromString(@"baseURL");
+    IMP urlImp = [apiClient methodForSelector:urlSelector];
+    NSString* (*urlFunc)(id, SEL) = (NSString* (*)(id, SEL))urlImp;
+    return urlFunc(apiClient, urlSelector);
+}
+
+- (NSString *)getAppId {
+    Class stateHandlerClass = objc_getClass("CPStateHandler");
+    
+    SEL sharedSelector = NSSelectorFromString(@"shared");
+    IMP sharedImp = [stateHandlerClass methodForSelector:sharedSelector];
+    id (*sharedFunc)(id, SEL) = (id (*)(id, SEL))sharedImp;
+    id stateHandler = sharedFunc(stateHandlerClass, sharedSelector);
+    
+    SEL sdkKeySelector = NSSelectorFromString(@"sdkKey");
+    IMP sdkKeyImp = [stateHandler methodForSelector:sdkKeySelector];
+    id (*sdkKeyFunc)(id, SEL) = (id (*)(id, SEL))sdkKeyImp;
+    id sdkKey = sdkKeyFunc(stateHandler, sdkKeySelector);
+    
+    SEL keySelector = NSSelectorFromString(@"key");
+    IMP keyImp = [sdkKey methodForSelector:keySelector];
+    NSString* (*keyFunc)(id, SEL) = (NSString* (*)(id, SEL))keyImp;
+    return keyFunc(sdkKey, keySelector);
+}
+
+- (NSString *)getDeviceId {
+    Class deviceClass = objc_getClass("Marigold.Device");
+    
+    SEL sharedSelector = NSSelectorFromString(@"shared");
+    IMP sharedImp = [deviceClass methodForSelector:sharedSelector];
+    id (*sharedFunc)(id, SEL) = (id (*)(id, SEL))sharedImp;
+    id device = sharedFunc(deviceClass, sharedSelector);
+    
+    SEL deviceIdSelector = NSSelectorFromString(@"deviceId");
+    IMP deviceIdImp = [device methodForSelector:deviceIdSelector];
+    NSString* (*deviceIdFunc)(id, SEL) = (NSString* (*)(id, SEL))deviceIdImp;
+    return deviceIdFunc(device, deviceIdSelector);
+}
+
+- (NSString *)getDevice {
+    Class builderClass = objc_getClass("Marigold.DeviceDictionaryBuilder");
+    id builder = [[builderClass alloc] init];
+    
+    SEL addAllSelector = NSSelectorFromString(@"addAll");
+    IMP addAllImp = [builder methodForSelector:addAllSelector];
+    id (*addAllFunc)(id, SEL) = (id (*)(id, SEL))addAllImp;
+    addAllFunc(builder, addAllSelector);
+    
+    SEL dictionarySelector = NSSelectorFromString(@"dictionary");
+    IMP dictionaryImp = [builder methodForSelector:dictionarySelector];
+    NSDictionary* (*dictionaryFunc)(id, SEL) = (NSDictionary* (*)(id, SEL))dictionaryImp;
+    NSDictionary *deviceDictionary = dictionaryFunc(builder, dictionarySelector);
+    
+    return [NSString stringWithFormat:@"%@", deviceDictionary];
 }
 
 @end
