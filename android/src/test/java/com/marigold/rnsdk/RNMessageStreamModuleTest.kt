79af0c6e5b2bd8316a5612f5ccb482483b3c0d80
“FrancisLotivio” <francis.lotivio@cheetahdigital.com>
2026-03-17 07:43:47 +0800

    [MP-2358] RN: Add support for full screen in-app customisation This PR
    ** Adds support for custom full screen inapp.
    
    https://sailthru.atlassian.net/browse/MP-2358

--- android/src/test/java/com/marigold/rnsdk/RNMessageStreamModuleTest.kt
+++ android/src/test/java/com/marigold/rnsdk/RNMessageStreamModuleTest.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.json.JSONException
 import org.json.JSONObject
+import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -58,12 +59,18 @@ class RNMessageStreamModuleTest {
 
     @Before
     fun setup() {
+        staticMessageStream = Mockito.mockConstruction(MessageStream::class.java)
         rnMessageStreamModule = RNMessageStreamModule(mockContext, true)
         rnMessageStreamModule.rnMessageStreamModuleImpl.jsonConverter = jsonConverter
         rnMessageStreamModuleImplSpy = Mockito.spy(rnMessageStreamModule.rnMessageStreamModuleImpl)
         rnMessageStreamModule.rnMessageStreamModuleImpl = rnMessageStreamModuleImplSpy
 
         messageStream = staticMessageStream.constructed()[0]
+    }
+
+    @After
+    fun tearDown() {
+        staticMessageStream.close()
     }
 
     @Test
