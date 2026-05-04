f780f4032b259572de2de933ecdfbf9d966ce2e1
Ian Stewart <istewart@sailthru.com>
2026-05-04 13:16:43 +1200

    [MP-2521] Fix JS tests

--- test/mocks/marigold-native-bridge.js
+++ test/mocks/marigold-native-bridge.js
@@ -3,3 +3,4 @@ import {NativeModules} from 'react-native';
 NativeModules.RNMarigold = {};
 NativeModules.RNMessageStream = {};
 NativeModules.RNEngageBySailthru = {};
+NativeModules.RNCheetah = {};
